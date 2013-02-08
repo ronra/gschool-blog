cc83c135589624a16c5caa92e6c1a15c5c4c9b34
Ron Rateau <ronra@ymail.com>
2013-02-08 09:28:42 -0700

    retro

---
+++ retroeventreporter.rb
@@ -0,0 +1,16 @@
+#A basic introduction to the project
+
+#A recap of your work pattern (days, hours, working alone / with others)
+days-started to work on the project on monday after 1pm
+hours-7 hours 12 hours 12 2 hours
+working alone 75% tried to work alone
+working with others: a few times, but when I had a question regarding the assignmenet
+#Link to your code repository
+github.com/ronra/event_reporter.git
+
+#Summary of your results/conclusions
+failed results due because of a wrong strategy. Next time, start early and ask questions early.
+#What worked and you’ll do the same next time
+Asking questions early
+#What didn’t work and you’ll change next time
+the hours of working alone. Next time look to meet with a group to working on projects
